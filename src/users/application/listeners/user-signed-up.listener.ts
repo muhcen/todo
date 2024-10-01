51aefe521a315e51d700852442bc3d043adae6ed
muhcen <mohsenun7@gmail.com>
2024-10-01 16:54:17 +0330

    remove saga from user module

--- src/users/application/listeners/user-signed-up.listener.ts
+++ src/users/application/listeners/user-signed-up.listener.ts
@@ -1,11 +1,11 @@
-// import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
-// import { UserSignedUpEvent } from '../events/user-signed-up.event';
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { UserSignedUpEvent } from '../events/user-signed-up.event';
 
-// @EventsHandler(UserSignedUpEvent)
-// export class UserSignedUpListener implements IEventHandler<UserSignedUpEvent> {
-//   handle(event: UserSignedUpEvent) {
-//     console.log(
-//       `User signed up with ID: ${event.userId}, Username: ${event.username}`,
-//     );
-//   }
-// }
+@EventsHandler(UserSignedUpEvent)
+export class UserSignedUpListener implements IEventHandler<UserSignedUpEvent> {
+  handle(event: UserSignedUpEvent) {
+    console.log(
+      `User signed up with ID: ${event.userId}, Username: ${event.username}`,
+    );
+  }
+}
